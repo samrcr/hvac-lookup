4fc0da0f62fb83cb9da726adab0334b89f697a2b
samrcr <sam.hannaford@rcrnz.co.nz>
2026-06-16 14:58:45 +1200

    Auto update Tue 16/06/2026 14:58:45.66

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,12 @@
+
 @echo off
 echo Deploying HVAC Lookup...
+set GIT="C:\Users\SamHannaford\AppData\Local\GitHubDesktop\app-3.5.12\resources\app\git\cmd\git.exe"
 cd /d "C:\Users\SamHannaford\OneDrive - RCR Infrastructure Limited\Sams apps\HVAC Lookup\hvac-lookup"
-git add .
-git commit -m "Auto update %date% %time%"
-git push
+%GIT% add .
+%GIT% commit -m "Auto update %date% %time%"
+%GIT% push
+echo.
 echo Done! Cloudflare will update in ~30 seconds.
 pause
+ 
